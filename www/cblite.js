a57811a158c051ee4f2be8991beeb1e7e9c2d0d3
Fernando Berti <bertilxi@gmail.com>
2017-11-03 15:01:28 -0300

    testing plugin

--- www/cblite.js
+++ www/cblite.js
@@ -1,14 +1,14 @@
-var PLUGIN_NAME = 'DataLayer';
+var PLUGIN_NAME = 'CBLite';
 
 var DataLayer = {
     echo: function (phrase, cb) {
         cordova.exec(cb, null, PLUGIN_NAME, 'echo', [phrase]);
     },
     getURL: function (callback) {
-        cordova.exec(function (url) { callback(false, url); }, function (err) { callback(err); }, "CBLite", "getURL", []);
+        cordova.exec(function (url) { callback(false, url); }, function (err) { callback(err); }, PLUGIN_NAME, "getURL", []);
     },
     getSamples: function (view, cb) {
-        cordova.exec(cb, null, PLUGIN_NAME, 'getSamples', [view]);
+        cordova.exec(cb, null, PLUGIN_NAME, 'getSamples', []);
     }
 };
 
